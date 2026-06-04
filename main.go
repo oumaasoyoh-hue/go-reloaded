6f3d916a57c921e0e57e9a8a7e959986e3a4a611
oumaasoyoh-hue <oumaasoyoh@gmail.com>
2026-06-04 04:23:40 +0300

    main.go changes

--- main.go
+++ main.go
@@ -6,21 +6,25 @@ import (
 )
 
 func main() {
-	if len(os.Args) < 3 {
-		fmt.Println("Usage: go run . <input_file> <output_file>")
-		return
-	}
-	inputFile := os.Args[1]
-	outputFile := os.Args[2]
-	content, err := os.ReadFile(inputFile)
-	if err != nil {
-		fmt.Printf("Error reading file: %v\n", err)
-		return
-	}
-	result := ProcessText(string(content))
-	err = os.WriteFile(outputFile, []byte(result), 0644)
-	if err != nil {
-		fmt.Println("Error writing file: %v\n")
+	if len(os.Args) != 3 {
+		fmt.Fprintln(os.Stderr, "Usage: go run . <input_file> <output_file>")
+		os.Exit(1)
 	}
 
+	inputFile := os.Args[1]
+	outputFile := os.Args[2]
+
+	content, err := os.ReadFile(inputFile)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error reading file: %v\n", err)
+		os.Exit(1)
+	}
+
+	result := ProcessText(string(content))
+
+	err = os.WriteFile(outputFile, []byte(result), 0644)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error writing file: %v\n", err)
+		os.Exit(1)
+	}
 }
